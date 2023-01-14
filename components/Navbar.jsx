d62d1e83c2990c2056aae3e384fc732449e4300d
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-01-15 01:00:51 +0600

    navbar startiing

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,21 @@
+import Image from 'next/image';
 import styles from '../styles/Navbar.module.css'
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
-            navbar
-        </div>
+           <div className={styles.item}>
+                <div className={styles.callButton}>
+                    <Image src="/img/telephone.png" alt="Telephone" width="32" height="32" />
+                </div>
+                <div className={styles.texts}>
+                    <div className={styles.text}>ORDER NOW!</div>
+                    <div className={styles.text}>013 0402 7758</div>
+                </div>
+           </div>
+           <div className={styles.item}>center</div>
+           <div className={styles.item}>right</div>
+           </div>
     );
 };
 
