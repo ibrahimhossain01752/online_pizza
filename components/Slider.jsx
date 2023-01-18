b4fa1da99bb475cae65865a4cf0183c34b66ed3e
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-01-19 00:48:26 +0600

    banner added

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,19 +1,36 @@
 import Image from "next/image";
-import styles from "../styles/Slider.module.css";
+// import styles from "../styles/Slider.module.css";
 
 const Slider = () => {
-    // const images = [
-    //     "/img/pizza.png",
-    //     "/img/featured2.png",
-    //     "/img/featured3.png",
-    // ];
     return (
-        <div className={styles.container}>
-            <Image src="/img/pizza.png" alt="" width="1000" height="1000" />
-            
-            {/* <Image src="/img/arrowr.png" alt=""  width="100" height="100"/> */}
-            
-        </div>
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Hello there</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+    //     <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    //         <div class="grid grid-cols-6 gap-4 md:container md:mx-auto" >
+       
+    //    <div class="col-start-1 col-end-3 ...">
+    //        <h4>HOT & SPICY</h4>
+    //        <h1>PIZZA</h1>
+    //        <hr/>
+    //        <br/>
+    //        <h4>50% OFF</h4>
+    //        <hr/>
+    //        <h5>ORDER NOW</h5>
+    //        <hr/>
+    //        <p>LAMA DEV</p>
+    //    </div>
+    //    <div class="col-end-7 col-span-2 ..."><Image src="/img/pizza.png" alt="" width="300" height="100"/></div>
+       
+    //  </div>
+    //     </div>
+       
     );
 };
 
