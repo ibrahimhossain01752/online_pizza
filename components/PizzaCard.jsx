817cdf21e62998bee25a28ffab22c67b539b7993
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-01-26 21:55:25 +0600

    pizza card added

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -2,7 +2,7 @@ const PizzaCard = () => {
     return (
         <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-5">
             <div className="card w-66 bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <figure><img src="https://i.ibb.co/cTxZf3V/pizza8.png" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -12,7 +12,7 @@ const PizzaCard = () => {
   </div>
 </div>
             <div className="card w-66 bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <figure><img src="https://i.ibb.co/Wnj5LDH/pizza7.png" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -22,7 +22,7 @@ const PizzaCard = () => {
   </div>
 </div>
             <div className="card w-66 bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <figure><img src="https://i.ibb.co/9ZvGrfM/pizza6.png" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -32,7 +32,47 @@ const PizzaCard = () => {
   </div>
 </div>
             <div className="card w-66 bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <figure><img src="https://i.ibb.co/6yfH2g4/pizza5.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+            <div className="card w-66 bg-base-100 shadow-xl">
+  <figure><img src="https://i.ibb.co/VN8ns6Z/pizza10-removebg-preview.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+            <div className="card w-66 bg-base-100 shadow-xl">
+  <figure><img src="https://i.ibb.co/rt9sR21/pizza3.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+            <div className="card w-66 bg-base-100 shadow-xl">
+  <figure><img src="https://i.ibb.co/9VGZnB0/pizza2.png" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+            <div className="card w-66 bg-base-100 shadow-xl">
+  <figure><img src="https://i.ibb.co/DfDfWMC/pizza1.png" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
