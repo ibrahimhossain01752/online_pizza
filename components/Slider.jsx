5e7344c7aa6f151cb8ef0cbea64ffd6c9cbf44fe
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-01-26 14:56:36 +0600

    banner resize

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,37 +1,27 @@
 import Image from "next/image";
-// import styles from "../styles/Slider.module.css";
+import styles from "../styles/Slider.module.css";
 
 const Slider = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
-    //     <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-    //         <div class="grid grid-cols-6 gap-4 md:container md:mx-auto" >
-       
-    //    <div class="col-start-1 col-end-3 ...">
-    //        <h4>HOT & SPICY</h4>
-    //        <h1>PIZZA</h1>
-    //        <hr/>
-    //        <br/>
-    //        <h4>50% OFF</h4>
-    //        <hr/>
-    //        <h5>ORDER NOW</h5>
-    //        <hr/>
-    //        <p>LAMA DEV</p>
-    //    </div>
-    //    <div class="col-end-7 col-span-2 ..."><Image src="/img/pizza.png" alt="" width="300" height="100"/></div>
-       
-    //  </div>
-    //     </div>
-       
-    );
+        <div  className="hero min-h-screen  bg_orange">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src="https://www.freeiconspng.com/thumbs/pizza-png/pizza-png-15.png"  className="banner_img rounded-lg" />
+          <div className="mb-44 mr-1">
+            <h4 className="text-2xl font-bold mx-14 text-white">HOT & SPICY</h4>
+            <h1 className="text-8xl font-bold text-white">PIZZA</h1>
+            <hr className="font-bold w-50 m-4"/>
+           <div className="my-10">
+           <h1 className="mx-20 text-white text-2xl">50% OFF</h1>
+            <hr className="w mx-auto"/>
+            <h4 className="text-white text-2xl mx-14">ORDER NOW</h4>
+            <hr/>
+            <h5 className="text-white text-1xl mx-20">LAMA DEV</h5>
+           </div>
+           </div>
+        </div>
+      </div>
+  
+       );
 };
 
 export default Slider;
